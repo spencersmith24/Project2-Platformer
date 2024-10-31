96af136733ced85934e08b113eb92f0edba56e6a
Spencer Smith <spencersmith24@outlook.com>
2024-10-30 20:09:14 -0400

    add checkpoint

--- platformer/Scripts/GameController.gd
+++ platformer/Scripts/GameController.gd
@@ -8,6 +8,10 @@ extends Node
 @onready var HIDDEN_LAYER = $"../TileMapLayers/HiddenLayer"
 @onready var PLAYER_START_POS = PLAYER.position
 
+# Checkpoint variables
+var has_checkpoint = false
+@onready var checkpoint_pos = GAME_OBJ.position
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,53 +19,39 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	#barrier_check()
 	obj_check()
-		
-func reset_level():
-	PLAYER.has_axe = false
-	PLAYER.has_obj = false
-	PLAYER.extra_jump = false
-	PLAYER.position = PLAYER_START_POS
-	
-	HIDDEN_LAYER.process_mode = Node.PROCESS_MODE_INHERIT
-	HIDDEN_LAYER.visible = true
-	HIDDEN_LAYER.collision_enabled = true
-	
-	GAME_AXE.process_mode = Node.PROCESS_MODE_INHERIT
-	GAME_AXE.visible = true
-	
-	BARRIER.process_mode = PROCESS_MODE_INHERIT
-	BARRIER.modulate.a = 1
-	
-	GAME_DJ.process_mode = Node.PROCESS_MODE_INHERIT
-	GAME_DJ.visible = true
-	
-	GAME_OBJ.process_mode = Node.PROCESS_MODE_INHERIT
-	GAME_OBJ.visible = true
 
-func barrier_check():
-	if PLAYER.has_axe:
-		BARRIER.process_mode = PROCESS_MODE_DISABLED
-		BARRIER.modulate.a = 0.25
-	else:
+func reset_level():
+	if has_checkpoint == false:
+		PLAYER.has_axe = false
+		PLAYER.has_obj = false
+		PLAYER.extra_jump = false
+		PLAYER.position = PLAYER_START_POS
+		
+		HIDDEN_LAYER.process_mode = Node.PROCESS_MODE_INHERIT
+		HIDDEN_LAYER.visible = true
+		HIDDEN_LAYER.collision_enabled = true
+		
+		GAME_AXE.process_mode = Node.PROCESS_MODE_INHERIT
+		GAME_AXE.visible = true
+		
 		BARRIER.process_mode = PROCESS_MODE_INHERIT
 		BARRIER.modulate.a = 1
-
+		
+		GAME_DJ.process_mode = Node.PROCESS_MODE_INHERIT
+		GAME_DJ.visible = true
+		
+		GAME_OBJ.process_mode = Node.PROCESS_MODE_INHERIT
+		GAME_OBJ.visible = true
+	else:
+		PLAYER.position = checkpoint_pos
 
 func obj_check():
 	if PLAYER.has_obj:
 		HIDDEN_LAYER.process_mode = Node.PROCESS_MODE_DISABLED
 		HIDDEN_LAYER.visible = false
 		HIDDEN_LAYER.collision_enabled = false
-
-
-func _on_flag_area_body_entered(body):
-	if body.is_in_group("Player") and PLAYER.has_flag:
-		PLAYER.has_flag = false
-		HIDDEN_LAYER.process_mode = Node.PROCESS_MODE_INHERIT
-		HIDDEN_LAYER.visible = true
-		print("WINNER WINNER CHICKEN DINNER")
+		has_checkpoint = true
 
 func _on_btm_bndry_body_entered(_body):
 	reset_level()
--- platformer/Scripts/market.gd
+++ platformer/Scripts/market.gd
@@ -6,6 +6,6 @@ func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		if body.has_obj:
 			body.has_obj = false
-			print("FART")
+			print("WINNER")
 	else:
 		pass
