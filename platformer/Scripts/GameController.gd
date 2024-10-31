734e1424eaeb278d9e76d4cc58c4b7f4aa0faf36
Spencer Smith <spencersmith24@outlook.com>
2024-10-31 16:22:43 -0400

    connect pumpkin returning to level progression

--- platformer/Scripts/GameController.gd
+++ platformer/Scripts/GameController.gd
@@ -6,11 +6,14 @@ extends Node
 @onready var GAME_DJ =  $"../lvlObjects/DoubleJumpItem"
 @onready var GAME_OBJ = $"../lvlObjects/Pumpkin"
 @onready var HIDDEN_LAYER = $"../TileMapLayers/HiddenLayer"
+@onready var GAME_MARKET = $"../lvlObjects/Market"
 @onready var PLAYER_START_POS = PLAYER.position
 
 # Checkpoint variables
 var has_checkpoint = false
 @onready var checkpoint_pos = GAME_OBJ.position
+
+@export var canContinue = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +22,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	
+	can_continue_check()
 	obj_check()
 
 func reset_level():
@@ -57,3 +62,18 @@ func obj_check():
 		
 func _on_btm_bndry_body_entered(_body):
 	reset_level()
+
+
+func _on_market_body_entered(body):
+	if body.is_in_group("Player"):
+		if body.has_obj:
+			GAME_MARKET.txtVisibility = true
+			body.has_obj = false
+			canContinue = true
+	else:
+		GAME_MARKET.txtVisibility = false
+
+func can_continue_check():
+	if Input.is_action_just_pressed("continue") and canContinue:
+		get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+		canContinue = false
--- platformer/Scripts/market.gd
+++ platformer/Scripts/market.gd
@@ -2,21 +2,8 @@ extends Area2D
 
 @export_multiline var text : String
 
-@export var canContinue = false
+var txtVisibility = false
 
 func _process(_delta):
+	$Label.visible = txtVisibility
 	$Label.text = text
-	
-	if Input.is_action_just_pressed("continue") and canContinue:
-		print("WINNER")
-		canContinue = false
-
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		if body.has_obj:
-			$Label.visible = true
-			body.has_obj = false
-			canContinue = true
-	else:
-		$Label.visible = false
-		pass
