ec16051776f8237abdcca351d24a7a452e770327
Spencer Smith <spencersmith24@outlook.com>
2024-10-31 15:03:44 -0400

    write continue script

--- platformer/Scripts/market.gd
+++ platformer/Scripts/market.gd
@@ -1,11 +1,22 @@
 extends Area2D
 
+@export_multiline var text : String
 
+@export var canContinue = false
+
+func _process(_delta):
+	$Label.text = text
+	
+	if Input.is_action_just_pressed("continue") and canContinue:
+		print("WINNER")
+		canContinue = false
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		if body.has_obj:
+			$Label.visible = true
 			body.has_obj = false
-			print("WINNER")
+			canContinue = true
 	else:
+		$Label.visible = false
 		pass
