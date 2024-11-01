0ffcf34e56cebc4a1a66efe51a89f2fa9c7996b0
Spencer Smith <spencersmith24@outlook.com>
2024-11-01 16:14:18 -0400

    create .exes

--- platformer/Scripts/credits.gd
+++ platformer/Scripts/credits.gd
@@ -3,7 +3,7 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("continue"):
-		get_tree().change_scene_to_file("res://Scenes/level1.tscn")
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 	
 	$ParallaxBackground/Clouds_1.motion_offset.x -= -15 * delta
 	$ParallaxBackground/Clouds_2.motion_offset.x += -30 * delta
